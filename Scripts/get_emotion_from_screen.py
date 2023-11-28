cdb5dff29416024e11ea3603f369e0a57df78c1a
nicchavez <nicolaschavez530@gmail.com>
2023-11-28 14:25:32 -0300

    adding line graph

--- Scripts/get_emotion_from_screen.py
+++ Scripts/get_emotion_from_screen.py
@@ -4,6 +4,7 @@ from pyautogui import screenshot
 import csv
 import numpy as np
 import keyboard
+from datetime import datetime
 
 def get_emotions(img):
     #print('height: ', img.shape[0], 'width: ', img.shape[1])
@@ -14,9 +15,10 @@ def get_emotions(img):
 
 def save_results(results):
     with open("data.csv", mode="a", newline='') as archivo_csv:
-        campos = ["emotion", "score"]
+        campos = ["datetime", "emotion", "score"]
         escritor_csv = csv.DictWriter(archivo_csv, campos)
-        rows = [{'emotion': re[0], 'score': re[1]} for re in results]
+        hora = datetime.now().strftime('%H:%M:%S') 
+        rows = [{'datetime':hora,'emotion': re[0], 'score': re[1]} for re in results]
         escritor_csv.writerows(rows)
         print("Data loaded")
 
@@ -30,6 +32,7 @@ def main():
         frame = np.array(img)
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         emotions = get_emotions(frame)
+        print(emotions)
         if len(emotions):
             count += 1
             print(f'{len(emotions)} faces detected, iteration number {count}')
